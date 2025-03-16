06432c2fcddcf78eaf90b04ccdcc4907b6bbe408
pavanpodila <pavan@vyuh.tech>
2025-03-16 18:16:19 +0530

    working initial version

--- packages/vyuh-react/src/index.ts
+++ packages/vyuh-react/src/index.ts
@@ -1,7 +1,6 @@
 // Core
 export * from './core/vyuh-provider';
 export * from './core/feature-descriptor';
-export * from './';
 export { useRestartPlatform } from '@/hooks/use-vyuh';
 export { useVyuh } from '@/hooks/use-vyuh';
 export { useVyuhStore } from '@/hooks/use-vyuh';
