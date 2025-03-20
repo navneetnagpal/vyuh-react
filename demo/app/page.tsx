e79a6bc7772e1260b6d0fad898a0a5f7cd2b8088
pavanpodila <pavan@vyuh.tech>
2025-03-20 07:46:47 +0530

    some fixes

--- demo/app/page.tsx
+++ demo/app/page.tsx
@@ -1,5 +1,5 @@
 import { RouteBuilder } from '@vyuh/react-extension-content';
 
 export default function Home() {
-  return <RouteBuilder url={'/react'} allowRefresh={true} />;
+  return <RouteBuilder url={'/chakra'} allowRefresh={true} />;
 }
