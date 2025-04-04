1bff899e9c0fe11f12c38cf08b9021065c6f90b0
pavanpodila <pavan@vyuh.tech>
2025-04-04 10:46:56 +0530

    going back to routeloader

--- apps/demo/app/page.tsx
+++ apps/demo/app/page.tsx
@@ -1,18 +1,8 @@
 'use client';
 
-import { DocumentLoader, fetchWithQuery } from '@vyuh/react-extension-content';
+import { RouteLoader } from '@vyuh/react-extension-content';
 
 export default function Home() {
   // Load the homepage route from CMS
-  return (
-    <DocumentLoader
-      fetchContent={fetchWithQuery(
-        '*[_type == "vyuh.route" && path == "/chakra"][0]',
-        {
-          live: true,
-        },
-      )}
-      allowRefresh={true}
-    />
-  );
+  return <RouteLoader url={'/chakra'} allowRefresh={true} live={true} />;
 }
